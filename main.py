b561196feaaa04112f193a7a78ada4281203c39d
Murid Quorishi <123528271+mqfazil@users.noreply.github.com>
2025-05-23 20:47:12 +0000

    day 2 of 100 days

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Getting to know you!")
+
+print()
+
+name = input("What's your name? ")
+food = input("What's your favorite food? ")
+music = input("What's your favorite music? ")
+live = input("Where do you live? ")
+
+print()
+
+print("You are: " + name)
+print("You're probably hungry for " + food)
+print("You're definitely listening to " + music)
+print("And you're living in " + live)
